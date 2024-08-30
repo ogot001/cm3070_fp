ed1b6c81382c41da55da455b8ef718303db5e82c
ogot001 <ogot001@mymail.sim.edu.sg>
2024-08-30 23:47:14 +0800

    updated front end form to dynamically handle fromFields.js
    updated front end form to dynamically handle array of fields & settings

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -40,6 +40,10 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/recordlist",
+    element: <RecordList />, // Record list page
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
---
+++ formFields.js
@@ -0,0 +1,13 @@
+const formFields = [
+    ["Name", "name", "text", true],
+    ["Date of Birth", "dob", "date", false],
+    ["IC Number", "icNumber", "text", false],
+    ["Email", "email", "email", false],
+    ["Mobile Number", "mobileNumber", "phone", false],
+    ["Position", "position", "text", false],
+    ["Level", "level", "dropdown", false, ["Intern", "Junior", "Senior"]],
+    // ["Department", "department", "dropdown", false, ["HR", "Engineering", "Marketing"]],
+  ];
+  
+export default formFields;
+  
