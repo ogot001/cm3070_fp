1a3dac62098a60079d129f1868b8e9c49fa2702a
ogot001 <ogot001@mymail.sim.edu.sg>
2024-09-08 00:41:48 +0800

    Update formFields.mjs

--- formFields.mjs
+++ formFields.mjs
@@ -1,16 +1,3 @@
-// const formFields = [
-//     ["Name", "name", "text", true],
-//     ["Date of Birth", "dob", "date", false],
-//     ["IC Number", "icNumber", "text", false],
-//     ["Email", "email", "email", false],
-//     ["Mobile Number", "mobileNumber", "phone", false],
-//     ["Position", "position", "text", false],
-//     ["Level", "level", "dropdown", false, ["Intern", "Junior", "Senior"]],
-//     ["Department", "department", "dropdown", false, ["HR", "Engineering", "Marketing"]],
-//   ];
-  
-// export default formFields;
-
 // const standardFields = [
 //   ["Created On", "created_on", "date", false],
 //   ["Created By", "created_by", "text", false],
